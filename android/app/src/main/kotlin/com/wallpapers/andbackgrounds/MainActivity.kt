43d6a296a8456cb4a02a739e42b65811d6a65761
belajargithub2 <belajar@ihsanbagus.com>
2023-05-18 22:58:31 +0700

    wallcat

--- android/app/src/main/kotlin/com/wallpapers/andbackgrounds/MainActivity.kt
+++ android/app/src/main/kotlin/com/wallpapers/andbackgrounds/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.wallpapers.natures
+package com.wallpapers.andbackgrounds
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/app/data/admob.dart
+++ lib/app/data/admob.dart
@@ -3,10 +3,10 @@ import 'package:wallpapers/app/data/randoms.dart';
 
 class Admob {
   static List<String> banners = [
-    'ca-app-pub-2024732515687909/8081989556',
+    'ca-app-pub-2024732515687909/2281726326',
   ];
   static List<String> interstitials = [
-    'ca-app-pub-2024732515687909/1545351425',
+    'ca-app-pub-2024732515687909/7787481672',
   ];
 
   static String getBannerId() {
