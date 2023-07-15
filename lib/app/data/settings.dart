603718815474e262db76b42235a6a3559d91b471
belajargithub2 <belajar@ihsanbagus.com>
2023-07-15 13:06:52 +0700

    add upgrader & startapp

--- lib/app/data/settings.dart
+++ lib/app/data/settings.dart
@@ -1,9 +1,9 @@
 import 'dart:ui';
 
-String appName = "Hogwarts Wallpapers HD";
-Color primaryColor = const Color(0xFF3F51B5);
-Color secondaryColor = const Color(0xFF303F9F);
-String search = "hogwarts";
+String appName = "Wonderland Wallpapers 4K";
+Color primaryColor = const Color(0xFF9097ff);
+Color secondaryColor = const Color(0xFF26274b);
+String search = "wonderland";
 String uriw = "JCsv4ex1mM78FAwRoH6Xj57CqhU14AULVq7eBVFylgw=";
 String urir = 'JCsv4ex1mM7nEBgU7XbRnY3Yxnpcm3grO8+9Dlp5nQc=';
 String mod = "IDoj+PwumoDgFAYMtm3cw5qEqhU14AULVq7eBVFylgw=";
