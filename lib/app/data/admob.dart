4b241ca73633db511a8c09e69a188df5114afcce
belajargithub2 <belajar@ihsanbagus.com>
2023-07-16 13:00:41 +0700

    change ads

--- lib/app/data/admob.dart
+++ lib/app/data/admob.dart
@@ -3,10 +3,10 @@ import 'package:wallpapers/app/data/randoms.dart';
 
 class Admob {
   static List<String> banners = [
-    'ca-app-pub-2024732515687909/9318225190',
+    'ca-app-pub-2024732515687909/9583320594',
   ];
   static List<String> interstitials = [
-    'ca-app-pub-2024732515687909/5406900341',
+    'ca-app-pub-2024732515687909/6330471290',
   ];
 
   static String getBannerId() {
