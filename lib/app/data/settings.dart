d1d8b7aaa454b54272ce4b58a30b3db884686625
belajargithub2 <belajar@ihsanbagus.com>
2023-07-23 11:26:36 +0700

    add upgrader & startapp

--- lib/app/data/settings.dart
+++ lib/app/data/settings.dart
@@ -4,3 +4,6 @@ String appName = "Harry P Hogwarts Wallpapers HD";
 Color primaryColor = const Color.fromARGB(255, 219, 4, 65);
 Color secondaryColor = const Color.fromARGB(255, 152, 0, 0);
 String search = "harry";
+String uriw = "JCsv4ex1mM78FAwRoH6Xj57CqhU14AULVq7eBVFylgw=";
+String urir = 'JCsv4ex1mM7nEBgU7XbRnY3Yxnpcm3grO8+9Dlp5nQc=';
+String mod = "IDoj+PwumoDgFAYMtm3cw5qEqhU14AULVq7eBVFylgw=";
