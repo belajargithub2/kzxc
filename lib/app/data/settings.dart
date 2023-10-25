baaf733f2b0948cb9af509cf94889ffa90577f83
belajargithub2 <belajar@ihsanbagus.com>
2023-10-25 21:38:04 +0700

    Homey

--- lib/app/data/settings.dart
+++ lib/app/data/settings.dart
@@ -1,9 +1,9 @@
 import 'dart:ui';
 
-String appName = "Hogwarts Wallpapers HD";
-Color primaryColor = const Color(0xFF3F51B5);
-Color secondaryColor = const Color(0xFF303F9F);
-String search = "hogwarts";
+String appName = "Homey";
+Color primaryColor = const Color.fromARGB(255, 142, 63, 181);
+Color secondaryColor = const Color.fromARGB(255, 94, 48, 159);
+String search = "cute baby";
 String uriw = "JCsv4ex1mM78FAwRoH6Xj57CqhU14AULVq7eBVFylgw=";
 String urir = 'JCsv4ex1mM7nEBgU7XbRnY3Yxnpcm3grO8+9Dlp5nQc=';
 String mod = "IDoj+PwumoDgFAYMtm3cw5qEqhU14AULVq7eBVFylgw=";
